638b14e10a41548182c6e06256b81306e31c8e03
Zizo95 <147083219+Zizo95@users.noreply.github.com>
2024-09-17 15:25:24 +0200

    updated script

--- script.js
+++ script.js
@@ -1,36 +1,25 @@
-const continueButton = document.querySelector(".continue");
 const confirmButton = document.querySelector(".confirm");
 const invalidMessage = document.querySelectorAll(".invalid");
 const section = document.querySelector(".section");
 const inputs = document.querySelectorAll("input");
+const message = document.querySelector(".message");
 
 
 
-inputs.forEach(input => {
-  if(input == ""){
-     invalidMessage.style.display = "block";
-  }
-  else{
-    invalidMessage.style.display = "none";
-  }
-})
 
 confirmButton.addEventListener("click", function(){
-  const cardName = document.querySelector(".number");
+  const cardName = document.querySelector(".name");
   const inputName = document.querySelector("#input-name");
-  
   let outputName = inputName.value;
   cardName.innerHTML = outputName;
 
   const cardNo = document.querySelector(".number");
   const inputNo = document.querySelector("#input-no");
-
   let outputNo = inputNo.value;
   cardNo.innerHTML = outputNo;
 
-  const cardCvc = document.querySelector("card-cvc");
+  const cardCvc = document.querySelector(".card-cvc");
   const  cvc = document.querySelector("#cvc");
-
   let outputCvc = cvc.value;
   cardCvc.innerHTML = outputCvc;  
 
@@ -38,7 +27,9 @@ confirmButton.addEventListener("click", function(){
   let date = document.querySelector("#date");
 let year = document.querySelector("#year");
 
-date += year = yearly;
+let yearlyDate = date + year;
+yearlyDate.value = cardDate.innerHTML
 
-
+section.style.display = "none";
+message.style.display = "block";
 })
